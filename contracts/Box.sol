1eb0e1e4001764c131815601f25aa5c50e1c60e7
Stephen G. Novosel, JD <sgnovo8@gmail.com>
2022-09-07 11:48:31 -0400

    flatten Box.sol

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,22 +1,3 @@
-// contracts/Box.sol
-// SPDX-License-Identifier: MIT
-// check correct solidity version
-pragma solidity ^0.8.0;
+// Sources flattened with hardhat v2.10.2 https://hardhat.org
 
-contract Box {
-    uint256 private _value;
-
-    // Emitted when the stored value changes
-    event ValueChanged(uint256 value);
-
-    // Stores a new value in the contract
-    function store(uint256 value) public {
-        _value = value;
-        emit ValueChanged(value);
-    }
-
-    // Reads the last stored value
-    function retrieve() public view returns (uint256) {
-        return _value;
-    }
-}
+// File contracts/Box.sol
